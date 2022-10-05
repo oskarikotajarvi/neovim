06f8c6b405da787a6b99f565482a51186925f801
Oskari <okotajarvi@topcon.com>
2022-10-05 08:57:03 +0300

    Created default flags for lsp servers

---
+++ lua/plugin-rc/lsp/general/default_flags.lua
@@ -0,0 +1,5 @@
+local default_flags = {
+    debounce_text_changes = 150,
+}
+
+return default_flags
--- lua/plugin-rc/lsp/settings/tsserver.lua
+++ lua/plugin-rc/lsp/settings/tsserver.lua
@@ -1,11 +1,9 @@
 local M = {}
 
 local on_attach = require("plugin-rc.lsp.general.on_attach")
+local flags = require("plugin-rc.lsp.general.default_flags")
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local flags = {
-	debounce_text_changes = 150,
-}
 local settings = {}
 
 M.name = "tsserver"
